a901dc4896c67b1560f462cfe485c018953cbbc2
Nishal C R <nishalcr@gmail.com>
2018-11-11 10:25:28 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,56 @@
+var express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose"),
+    Campground = require("./models/campground"),
+    Comment = require("./models/comment"),
+    seedDB = require("./seeds"),
+    flash = require("connect-flash"),
+    passport = require("passport"),
+    LocalStrategy = require("passport-local"),
+    User = require("./models/user"),
+    methodOverride = require("method-override");
+
+var campgroundRoutes = require("./routes/campgrounds"),
+    commentRoutes = require("./routes/comments"),
+    indexRoutes = require("./routes/index");
+
+
+mongoose.connect('mongodb://localhost:27017/yelp_camp_final', { useNewUrlParser: true });
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
+app.use(flash());
+
+//seedDB();
+
+app.use(require("express-session")({
+    secret: "MUFC IS THE BEST  FOOTBALL CLUB",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.locals.moment = require('moment');
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next) {
+    res.locals.currentUser = req.user;
+    res.locals.error = req.flash("error");
+    res.locals.success = req.flash("success");
+    next();
+});
+
+app.use(indexRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/comments", commentRoutes);
+
+
+app.listen(process.env.PORT, process.env.IP, function() {
+    console.log("The YelpCamp Server has started!");
+});
