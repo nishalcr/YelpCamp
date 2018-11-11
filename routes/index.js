4fb10c82a5b9b83315ccc8f58f4092c3265a9ca9
Nishal C R <nishalcr@gmail.com>
2018-11-11 14:20:36 +0000

    add user profile

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require("express");
 var router = express.Router();
 var passport = require("passport");
 var User = require("../models/user");
+var Campground = require("../models/campground");
 
 //root route
 router.get("/", function(req, res) {
@@ -15,10 +16,18 @@ router.get("/register", function(req, res) {
 
 //handle sign up logic
 router.post("/register", function(req, res) {
-    var newUser = new User({ username: req.body.username });
-    if(req.body.adminCode === 'yelpcampncr') {
-      newUser.isAdmin = true;
+    var newUser = new User({
+        username: req.body.username,
+        firstname: req.body.firstName,
+        lastname: req.body.lastName,
+        email: req.body.email,
+        avatar: req.body.avatar
+    });
+
+    if (req.body.adminCode === 'yelpcampncr') {
+        newUser.isAdmin = true;
     }
+
     User.register(newUser, req.body.password, function(err, user) {
         if (err) {
             console.log(err);
@@ -51,6 +60,22 @@ router.get("/logout", function(req, res) {
     res.redirect("/campgrounds");
 });
 
+// user profiles
+router.get("/users/:id", function(req, res) {
+    User.findById(req.params.id, function(err, foundUser) {
+        if (err) {
+            req.flash("error", "Something went wrong.");
+            return res.redirect("/");
+        }
 
+        Campground.find().where('author.id').equals(foundUser._id).exec(function(err, campgrounds) {
+            if (err) {
+                req.flash("error", "Something went wrong.");
+                return res.redirect("/");
+            }
+            res.render("users/show", { user: foundUser, campgrounds: campgrounds });
+        });
+    });
+});
 
 module.exports = router;
