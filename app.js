efc351a3ba6df8261fb24acdb4ade8383666d861
Nishal C R <nishalcr@gmail.com>
2018-11-12 05:24:27 +0000

    add password reset

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('dotenv').load();
+
 var express = require("express"),
     app = express(),
     bodyParser = require("body-parser"),
@@ -16,7 +18,7 @@ var campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes = require("./routes/index");
 
 
-mongoose.connect('mongodb://localhost:27017/yelp_camp', { useNewUrlParser: true });
+mongoose.connect('mongodb://localhost:27017/yelp_camp_final', { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
