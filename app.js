6c7bb27aafd4ab49815faa82c5428c89ecaed6d7
nishalcr <nishalcr@gmail.com>
2019-09-28 14:04:41 +0530

    Added: Error message for MongoDB Connection

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ mongoose.connect(process.env.MONGO_DB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).catch(err => {
-  console.log("MongoDB Connection failed !!! "+ err);
+  console.log("MongoDB Connection failed !!! "+ err.message);
 });
 
 app.use(bodyParser.urlencoded({ extended: true }));
